ef6dc7c04a4f1ad915af5f110f17b3c92eeb8853
klomucaj <klodimucaj8@gmail.com>
2025-07-07 10:24:54 +0200

    creation of the project with some updates

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "20bf149a2b8b73854e4a3deead84e215";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await  response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await  response.json()
+    return data.results
+};
